747f0a082ce8706e7fb19b450b565995a09dfdc5
zurachu <zurachu@gmail.com>
2017-07-20 17:54:49 +0900

    登録済みハッシュならリダイレクトするようにしてみる

--- index.php
+++ index.php
@@ -1,8 +1,33 @@
 <?php
+$db_url = parse_url(getenv('DATABASE_URL'));
 
 if(count($_GET) == 1)
 {
-	var_dump($_GET);
+	$key = key($_GET);
+	$value = $_GET[$key];
+	try {
+		$dsn = sprintf('pgsql:host=%s;dbname=%s', $db_url['host'], substr($db_url['path'], 1));
+		$pdo = new PDO($dsn, $db_url['user'], $db_url['pass']);
+
+		if($key == "source_url")
+		{
+			// 新規登録処理
+			echo $value;
+		}
+		else if($value == "")
+		{
+			// ハッシュとみなして登録済み情報を検索
+			$statement = foreach($pdo->query("SELECT url from url_conversion where hash = '$key'");
+			if($result = $statement->fetch())
+			{
+				header("Location: {$result['url']}");
+				exit;
+			}
+		}
+	} catch(PDOException $e) {
+		print($e->getMessage());
+		die($e->getMessage());
+	}
 	exit;
 }
 
