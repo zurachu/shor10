492dc34ddaa8bc8b02eeae66c5044dd75ec27bce
zurachu <zurachu@gmail.com>
2017-07-20 15:31:40 +0900

    url_conversion テーブルの中身全てを出力する

--- pdo_test.php
+++ pdo_test.php
@@ -5,6 +5,10 @@ try {
 	$dsn = sprintf('pgsql:host=%s;dbname=%s', $db_url['host'], substr($db_url['path'], 1));
 	$pdo = new PDO($dsn, $db_url['user'], $db_url['pass']);
 	var_dump($pdo->getAttribute(PDO::ATTR_SERVER_VERSION));
+	foreach($dbh->query("SELECT * from url_conversion") as $row) {
+		var_dump($row['hash']);
+		var_dump($row['url']);
+	}
 } catch(PDOException $e) {
-    die($e->getMessage());
+	die($e->getMessage());
 }
